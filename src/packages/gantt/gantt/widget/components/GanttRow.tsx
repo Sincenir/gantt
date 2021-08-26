f4877b953a7b916f4b80a6a717505c88109f73cd
Sincenir <843398723@qq.com>
2021-08-26 18:58:06 +0800

    滑块动起来啦！

--- src/packages/gantt/gantt/widget/components/GanttRow.tsx
+++ src/packages/gantt/gantt/widget/components/GanttRow.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { getDateInterval } from "../../../../util/date";
 import { RowData } from "../../../root";
 
 const GanttRow: React.FC<GanttRowProps> = (props) => {
   const { rowHeight, ganttColumnWidth, rowData, projectStart } = props;
-  console.log(getDateInterval(rowData.startDate as Date, rowData.endDate as Date) / 86400000);
+
+  const [left, setLeft] = useState(
+    (getDateInterval(projectStart, rowData.startDate as Date) / 86400000) *
+      ganttColumnWidth
+  );
+
+  const handleMove = (e: any) => {
+    const srcX = e.pageX;
+    document.onmousemove = (e: any) => {
+      const targetX = e.pageX;
+      setLeft(left + (targetX - srcX))
+      console.log(targetX, srcX);
+
+      document.onmouseup = () => {
+        document.onmousemove = document.onmouseup = null;
+      };
+    };
+  };
   return (
     <div
       style={{
@@ -18,11 +35,20 @@ const GanttRow: React.FC<GanttRowProps> = (props) => {
         style={{
           height: `${rowHeight - 10}px`,
           margin: "5px 0",
-          marginLeft: `${getDateInterval(projectStart,rowData.startDate as Date) / 86400000 * ganttColumnWidth}px`,
-          width: `${(getDateInterval(rowData.startDate as Date, rowData.endDate as Date) / 86400000 + 1) * ganttColumnWidth}px`,
+          marginLeft: `${left}px`,
+          width: `${
+            (getDateInterval(
+              rowData.startDate as Date,
+              rowData.endDate as Date
+            ) /
+              86400000 +
+              1) *
+            ganttColumnWidth
+          }px`,
           backgroundColor: "#93B5C6",
           borderRadius: "4px",
         }}
+        onMouseDown={handleMove}
       ></div>
     </div>
   );
